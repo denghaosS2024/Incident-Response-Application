8736ef4252296671458c77b7ccc944cf538f1538
Celina Cywinska <ccywinsk@andrew.cmu.edu>
2025-02-23 22:18:03 -0800

    SEM-2390 UI Skeleton for 911 Step 1

---
+++ client/src/components/Reach911Step1.tsx
@@ -0,0 +1,27 @@
+import {
+    Box,
+    TextField,
+} from '@mui/material'
+import styles from '../styles/Reach911Page.module.css'
+
+
+const Reach911Step1 = () => {
+
+    return (
+        <div className={styles.wrapperStep1}>
+            <div>
+                You have reached 911.
+                <p className={styles.bold}>
+                    Enter emergency address:
+                </p>
+            </div>
+            <Box width="100%" maxWidth="500px">
+                <TextField id="outlined-basic" label="Address" variant="outlined" />
+            </Box>
+            <p>PLACEHOLDER FOR MAP</p>
+            <p>Refine position by pressing the incident icon</p>
+        </div>
+    );
+};
+
+export default Reach911Step1;
