ca25e9828f75849644247faff23b65ce6cbcc6af
Celina Cywinska <ccywinsk@andrew.cmu.edu>
2025-02-24 15:52:09 -0800

    SEM-2389 Added ClickableStepper story

---
+++ client/src/stories/mui/ClickableStepper.stories.tsx
@@ -0,0 +1,51 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+import ClickableStepper from '../../components/ClickableStepper';
+import { Box, Typography } from '@mui/material';
+
+const meta: Meta<typeof ClickableStepper> = {
+    title: 'Material UI/Stepper',
+    component: ClickableStepper,
+    tags: ['autodocs'],
+    argTypes: {
+        numberOfSteps: {
+            control: { type: 'number', min: 1 },
+            description: 'Total number of steps in the stepper.',
+        },
+        activeStep: {
+            control: { type: 'number', min: 0 },
+            description: 'Currently active step.',
+        },
+        contents: {
+            control: { type: 'array', of: 'jsxElement' },
+            description: 'Array of React components to be displayed. One for each step.',
+        }
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const DefaultStepper: Story = {
+    args: {
+        numberOfSteps: 4,
+        activeStep: 0,
+    },
+    render: ({ numberOfSteps, activeStep }) => {
+        const [currentStep, setCurrentStep] = useState<number>(activeStep);
+        const contents: JSX.Element[] = [
+            <Box key={0}><Typography>React Component for Step 1</Typography></Box>,
+            <Box key={1}><Typography>React Component for Step 2</Typography></Box>,
+            <Box key={2}><Typography>React Component for Step 3</Typography></Box>,
+        ];
+
+        return (
+            <ClickableStepper
+                numberOfSteps={numberOfSteps}
+                activeStep={currentStep}
+                setActiveStep={setCurrentStep}
+                contents={contents}
+            />
+        );
+    },
+};
