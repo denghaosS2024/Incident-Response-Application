80eb8e8adef7c4e7dc3bc2492fc342eb4b2e2422
sonyemehS2024 <155502044+sonyemehS2024@users.noreply.github.com>
2025-03-07 16:46:58 -0800

    sem-2394, SEM-2395, SEM-2396, and SEM-2397 Added stories for MUI Checkbox and RadioGroup

---
+++ client/src/stories/mui/Checkbox.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Checkbox, CheckboxProps } from '@mui/material';
+
+const meta: Meta<CheckboxProps> = {
+    title: 'Material UI/Checkbox',
+    component: Checkbox,
+    tags: ['autodocs'],
+    argTypes: {
+        checked: {
+            control: 'boolean', 
+            description: 'Whether the checkbox is checked',
+        },
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const ClickableCheckbox: Story = {
+    args: {
+        checked: false,
+    },
+    render: (args) => {
+        return <Checkbox {...args} />; 
+    },
+};
