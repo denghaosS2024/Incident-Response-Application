31bd4157aa88d660ef197d67d1faa46e969312c6
taomingl-cmu-F24 <taomingl@andrew.cmu.edu>
2025-03-10 02:29:09 -0700

    SEM 2408 add assign personnel city api

--- server/src/controllers/PersonnelController.ts
+++ server/src/controllers/PersonnelController.ts
@@ -1,3 +1,4 @@
+import City from "../models/City";
 import User, { IUser } from "../models/User";
 import { ROLES } from "../utils/Roles";
 
@@ -19,6 +20,41 @@ class PersonnelController {
     }
   }
 
+  /**
+   * Update the assigned city for a specific personnel.
+   * @param username - The username of the personnel to update.
+   * @param cityName - The name of the city to assign.
+   * @returns The updated personnel object.
+   */
+  async updatePersonnelCity(username: string, cityName: string): Promise<IUser | null> {
+    try {
+      if (!cityName) {
+        const updatedPersonnel = await User.findOneAndUpdate(
+          {
+            username,
+            role: { $in: [ROLES.POLICE, ROLES.FIRE] },
+          },
+          { assignedCity: null },
+          { new: true }
+        )
+        return updatedPersonnel
+      }
+      const cityExists = await City.findOne({ name: cityName })
+      if (!cityExists) {
+        throw new Error(`City '${cityName}' does not exist in the database`)
+      }
+      const personnel = await User.findOne({ username })
+      if (!personnel || (personnel.role !== ROLES.POLICE && personnel.role !== ROLES.FIRE)) {
+        throw new Error(`Personnel with username '${username}' does not exist`)
+      }
+      const updatedPersonnel = await User.findOneAndUpdate(
+        { username }, { assignedCity: cityName })
+      return updatedPersonnel
+    } catch (error) {
+      console.error('Error updating personnel city:', error)
+      throw error
+    }
+  }
 }
 
 export default new PersonnelController();
