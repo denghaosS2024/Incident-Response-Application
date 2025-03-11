d9c1ca15580597e0665d010fa31eed3e1bf7d788
Michelle-YijingZhou <yijingz2@andrew.cmu.edu>
2025-03-11 15:39:07 -0700

    fix 911 icon

--- client/src/components/common/RoleIcon.tsx
+++ client/src/components/common/RoleIcon.tsx
@@ -1,16 +1,18 @@
-import * as React from "react";
 import {
-    ReportProblem as ReportProblemIcon,
-    LocalPolice as PoliceIcon,
     LocalFireDepartment as FireIcon,
     LocalHospital as NurseIcon,
+    LocalPolice as PoliceIcon,
 } from "@mui/icons-material";
 
 
 export const getRoleIcon = (role: string): JSX.Element | null => {
     switch (role) {
         case 'Dispatch':
-            return <ReportProblemIcon sx={{ color: 'red', marginRight: '8px' }} />;
+            return <img
+                        src="/911-icon-red.png"
+                        alt="Red 911 Icon"
+                        style={{ width: '28px', height: '28px', borderRadius: '8px' }}
+                    />;
         case 'Police':
             return <PoliceIcon sx={{ color: 'red', marginRight: '8px' }} />;
         case 'Fire':
