0c6a7f186797abd282a81c5dc9460a4243b6fdd0
Gary Chen <honghao2@andrew.cmu.edu>
2025-03-12 23:07:06 -0700

    SEM-2342: update AddGroupForm to fit new page

--- client/src/components/AddGroupForm.tsx
+++ client/src/components/AddGroupForm.tsx
@@ -1,15 +1,9 @@
-import { Fragment, FunctionComponent, ReactElement, useState, useEffect } from 'react'
-import AddIcon from '@mui/icons-material/Add'
+import React, { FunctionComponent, useState, useEffect } from 'react'
 import {
   Box,
   Button,
-  Divider,
   FormControlLabel,
   Link,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
   Switch,
   TextField,
   Typography,
@@ -19,21 +13,6 @@ import Board from "./Board"
 import IChannel from '../models/Channel'
 import {isSystemGroup} from "../utils/SystemDefinedGroups"
 
-
-interface ITab {
-  text: string
-  link: string
-  icon: ReactElement
-}
-
-// todo: should be refactored into a separate component as TabList
-const tabs: ITab[] = [
-  {
-    text: 'Create/Edit Group',
-    link: '/groups',
-    icon: <AddIcon />,
-  },
-]
 
 interface IFormData {
   name: string
@@ -55,10 +34,9 @@ export interface IAddGroupFormProps {
 const AddGroupForm: FunctionComponent<IAddGroupFormProps> = (
   channelProps: IAddGroupFormProps,
 ) => {
-  const [showForm, setShowForm] = useState(false)
-  const [closed, setIsClosed] = useState<boolean>(false)
-  const [name, setGroupName] = useState('')
-  const [description, setDescription] = useState('')
+  const [closed, setIsClosed] = useState<boolean>(channelProps.currentGroup?.closed || false)
+  const [name, setGroupName] = useState(channelProps.currentGroup?.name || '')
+  const [description, setDescription] = useState(channelProps.currentGroup?.description || '')
   const [nameError, setNameError] = useState<string>('')
   const owner = localStorage.getItem('uid') || ''
   const currentUsername = localStorage.getItem('username')
@@ -119,11 +97,6 @@ const AddGroupForm: FunctionComponent<IAddGroupFormProps> = (
     setIsClosed(event.target.checked)
   }
 
-  const handleCancelClick = () => {
-    resetForm()
-    setShowForm(false)
-  }
-
   const handleDeleteClick = () => {
     setNameError('')
     if (!name.trim()) {
@@ -142,13 +115,6 @@ const AddGroupForm: FunctionComponent<IAddGroupFormProps> = (
   const handleRemoveCurrentUserFromGroup = () => {
     channelProps.removeCurrentUserFromGroup()
     resetForm()
-  }
-
-  const handleAddGroupClick = (
-    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
-  ) => {
-    event.preventDefault()
-    setShowForm(true)
   }
 
   const handleGroupClickInBoard = (group: IChannel) => {
@@ -173,115 +139,99 @@ const AddGroupForm: FunctionComponent<IAddGroupFormProps> = (
       width: "100%",
       mx: "auto"
     }}>
-      <List>
-        {tabs.map(({ text, link, icon }, index) => (
-          <Fragment key={link}>
-            <Link color="inherit" href={link} onClick={handleAddGroupClick}>
-              <ListItem button>
-                <ListItemIcon>{icon}</ListItemIcon>
-                <ListItemText>{text}</ListItemText>
-              </ListItem>
-            </Link>
-
-            {index !== tabs.length - 1 && <Divider />}
-          </Fragment>
-        ))}
-      </List>
-
-      {showForm && (
-        <Box component="form" sx={{ mt: 2, mx: 2, mb: 2 }}>
-          <TextField
-            label="Group Name"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-            error={!!nameError}
-            helperText={nameError}
-            required
-            value={name}
-            onChange={(e) => setGroupName(e.target.value)}
+      <Box component="form" sx={{ mt: 2, mx: 2, mb: 2 }}>
+        <TextField
+          label="Group Name"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          error={!!nameError}
+          helperText={nameError}
+          required
+          value={name}
+          onChange={(e) => setGroupName(e.target.value)}
+        />
+        <TextField
+          label="Description"
+          variant="outlined"
+          fullWidth
+          value={description}
+          margin="normal"
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        {allowEdit && <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          mt={2}
+        >
+          <Typography variant="body1" sx={{mr: 2}}>
+            Owner: {currentUsername}
+          </Typography>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={closed}
+                onChange={handleToggleChange}
+                name="closed"
+                color="primary"
+              />
+            }
+            label="Closed"
           />
-          <TextField
-            label="Description"
-            variant="outlined"
-            fullWidth
-            value={description}
-            margin="normal"
-            onChange={(e) => setDescription(e.target.value)}
-          />
-          {allowEdit && <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="space-between"
-            mt={2}
+        </Box>}
+        <Board setUsers={setUsers} onGroupClick={handleGroupClickInBoard} triggerResetBoard={triggerResetBoard} canDrag={allowEdit} />
+        <Box display="flex" justifyContent="center" mt={2}>
+          {allowRemoveSelf && <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault()
+              handleRemoveCurrentUserFromGroup()
+            }}
+            sx={{mt: 2, mx: 1}}
           >
-            <Typography variant="body1" sx={{mr: 2}}>
-              Owner: {currentUsername}
-            </Typography>
-            <FormControlLabel
-              control={
-                <Switch
-                  checked={closed}
-                  onChange={handleToggleChange}
-                  name="closed"
-                  color="primary"
-                />
-              }
-              label="Closed"
-            />
-          </Box>}
-          <Board setUsers={setUsers} onGroupClick={handleGroupClickInBoard} triggerResetBoard={triggerResetBoard} canDrag={allowEdit} />
-          <Box display="flex" justifyContent="center" mt={2}>
-            {allowRemoveSelf && <Button
-              variant="contained"
-              color="primary"
-              type="submit"
-              onClick={(e) => {
-                e.preventDefault()
-                handleRemoveCurrentUserFromGroup()
-              }}
-              sx={{mt: 2, mx: 1}}
-            >
-              Remove Self
-            </Button>}
-            {allowEdit && <Button
-              variant="contained"
-              color="primary"
-              type="submit"
-              onClick={(e) => {
-                e.preventDefault()
-                handleSubmit()
-              }}
-              sx={{mt: 2, mx: 1}}
-            >
-              {(channelProps.currentGroup == null) ? "Create" : "Edit"}
-            </Button>}
-            <Button
-              variant="outlined"
-              color="primary"
-              onClick={handleCancelClick}
-              sx={{ mt: 2, mx: 1 }}
-            >
-              Cancel
-            </Button>
-            {allowEdit && <Button
-              variant="outlined"
-              color="primary"
-              onClick={handleDeleteClick}
-              sx={{mt: 2, mx: 1}}
-            >
-              Delete
-            </Button>}
-            <ConfirmationDialog
-              open={openConfirmDialog}
-              title="Delete Group"
-              description="Are you sure you want to delete this group?"
-              onConfirm={handleDeleteChannel}
-              onCancel={() => setOpenConfirmDialog(false)}
-            />
-          </Box>
+            Remove Self
+          </Button>}
+          {allowEdit && <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault()
+              handleSubmit()
+            }}
+            sx={{mt: 2, mx: 1}}
+          >
+            {(channelProps.currentGroup == null) ? "Create" : "Edit"}
+          </Button>}
+          <Button
+            component={Link}
+            href="/groups"
+            variant="outlined"
+            color="primary"
+            sx={{ mt: 2, mx: 1 }}
+          >
+            Cancel
+          </Button>
+          {allowEdit && <Button
+            variant="outlined"
+            color="primary"
+            onClick={handleDeleteClick}
+            sx={{mt: 2, mx: 1}}
+          >
+            Delete
+          </Button>}
+          <ConfirmationDialog
+            open={openConfirmDialog}
+            title="Delete Group"
+            description="Are you sure you want to delete this group?"
+            onConfirm={handleDeleteChannel}
+            onCancel={() => setOpenConfirmDialog(false)}
+          />
         </Box>
-      )}
+      </Box>
     </Box>
   )
 }
