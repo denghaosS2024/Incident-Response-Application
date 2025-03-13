1b21759bc1f8d291a9369be01bc456d2f2822fcc
Gary Chen <honghao2@andrew.cmu.edu>
2025-03-12 22:45:29 -0700

    SEM-2342: move create/edit group form to GroupInformationPage

--- client/src/pages/GroupsPage.tsx
+++ client/src/pages/GroupsPage.tsx
@@ -1,169 +1,39 @@
-import React, { useState } from 'react'
-import AddGroupForm from '../components/AddGroupForm'
-import request, { IRequestError } from '../utils/request'
-import IChannel from '../models/Channel'
-import { IAddGroupFormProps } from '@/components/AddGroupForm'
-import AlertSnackbar from '../components/common/AlertSnackbar'
-import style from '../styles/GroupPage.module.css'
-import SocketClient from '../utils/Socket'
+import React from 'react'
+import {Container, Fab, Link} from '@mui/material'
+import AddIcon from "@mui/icons-material/Add"
 
 //Pages
 import GroupDirectory from '../components/GroupDir/GroupDirectory'
-import { Container } from '@mui/material'
+
 
 const Groups: React.FC = () => {
-  const [errorMessage, setErrorMessage] = useState<string | null>(null)
-  const [openSnackbar, setOpenSnackbar] = useState(false)
-  const [successMessage, setSuccessMessage] = useState<string | ''>('')
-  const [currentGroup, setCurrentGroup] = useState<IChannel | null>(null);
-  const uid = localStorage.getItem('uid') || ''
-
-  // create or update channel
-  const newGroup: IAddGroupFormProps['createChannel'] = async ({
-    name,
-    description,
-    users,
-    owner,
-    closed,
-  }) => {
-    try {
-      setErrorMessage('')
-
-      let originalUsers: string[] = [];
-      let groupId = '';
-      let isUpdate = false;
-
-      if (currentGroup) {
-        // Update existing channel
-        groupId = currentGroup._id;
-        originalUsers = currentGroup.users
-          ? currentGroup.users.map(user => typeof user === 'object' ? user._id : user)
-          : [];
-
-        await request('/api/channels', {
-          method: 'PUT',
-          body: JSON.stringify({
-            _id: groupId,
-            name, description, users, owner, closed
-          }),
-        })
-
-        isUpdate = true;
-      } else {
-        // Create new channel
-        await request('/api/channels', {
-          method: 'POST',
-          body: JSON.stringify({
-            _id: undefined,
-            name, description, users, owner, closed
-          }),
-        })
-      }
-
-      if (isUpdate) {
-        const newUsers = users.filter(userId =>
-          !originalUsers.includes(userId) &&
-          userId !== owner
-        );
-
-        if (newUsers.length > 0) {
-          console.log(`Sending notifications to ${newUsers.length} new members:`, newUsers);
-
-          newUsers.forEach(userId => {
-            SocketClient.emit('group-member-added', {
-              userId: userId
-            });
-          });
-        }
-      }
-
-      setSuccessMessage('Group created or updated successfully!')
-      setOpenSnackbar(true)
-
-    } catch (e) {
-      const error = e as IRequestError
-      if (error.status >= 400 && error.status < 500) {
-        setErrorMessage(`Error: ${error.message}`)
-      } else {
-        setErrorMessage('Server error occurred. Please try again later.')
-      }
-      setOpenSnackbar(true)
-    }
-  }
-
-  const deleteGroup: IAddGroupFormProps['deleteChannel'] = async (
-    name: string,
-  ) => {
-    setErrorMessage('')
-    try {
-      // FIXME: should delete according to currentGroup._id instead of name
-      await request('/api/channels', {
-        method: 'DELETE',
-        body: JSON.stringify({ name }),
-      })
-      setSuccessMessage('Group deleted successfully!')
-      setOpenSnackbar(true)
-    } catch (e) {
-      const error = e as IRequestError
-      if (error.status >= 400 && error.status < 500) {
-        setErrorMessage(`Error: ${error.message}`)
-      } else {
-        console.log('error', error)
-        setErrorMessage('Server error occurred. Please try again later.')
-      }
-      setOpenSnackbar(true)
-    }
-  }
-
-  const removeCurrentUserFromGroup: IAddGroupFormProps['removeCurrentUserFromGroup'] = async () => {
-    setErrorMessage('')
-
-    if (currentGroup) {
-      const originalUsers = currentGroup.users
-        ? currentGroup.users.map(user => typeof user === 'object' ? user._id : user)
-        : [];
-      const usersWithoutCurrent = originalUsers.filter(userId => userId !== uid)
-
-      try {
-        await request('/api/channels', {
-          method: 'PUT',
-          body: JSON.stringify({
-            _id: currentGroup._id,
-            name: currentGroup.name,
-            description: currentGroup.description,
-            users: usersWithoutCurrent,
-            owner: currentGroup.owner._id,
-            closed: currentGroup.closed,
-          }),
-        })
-      } catch (e) {
-        const error = e as IRequestError
-        setErrorMessage(`Error: ${error.message}`)
-      }
-
-      setSuccessMessage('You are removed from the selected group.')
-      setOpenSnackbar(true)
-    }
-  }
-
 
   return (
     <Container>
-      <div className={style.centeredForm}>
-        <AddGroupForm
-          createChannel={newGroup}
-          deleteChannel={deleteGroup}
-          removeCurrentUserFromGroup={removeCurrentUserFromGroup}
-          currentGroup={currentGroup}
-          setCurrentGroup={setCurrentGroup} />
-      </div>
       <GroupDirectory />
-      <AlertSnackbar
-        open={openSnackbar}
-        onClose={() => setOpenSnackbar(false)}
-        message={errorMessage || successMessage}
-        severity={errorMessage ? 'error' : 'success'}
-      />
+
+      {/* Plus button for creating new group */}
+      <Link
+        href={'/groups/new'}
+        underline="none"
+        sx={{ display: 'inline-block' }}
+      >
+        <Fab
+          color="primary"
+          aria-label="add"
+          sx={{
+            position: 'fixed',
+            bottom: '20px',
+            right: '20px',
+            backgroundColor: '#1976d2', // primary blue color
+            '&:hover': {
+              backgroundColor: '#1565c0', // darker blue on hover
+            }}}
+        >
+          <AddIcon />
+        </Fab>
+      </Link>
+
     </Container>
   )
 }
