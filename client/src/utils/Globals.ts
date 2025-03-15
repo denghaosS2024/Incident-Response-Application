32cf45771b001b1eea4f3dbdbcef997f617f6997
baiyil-cmu-F24 <baiyil@andrew.cmu.edu>
2025-03-14 19:37:57 -0700

    feat: switch to tsx for backend deploy

--- client/src/utils/Globals.ts
+++ client/src/utils/Globals.ts
@@ -4,12 +4,12 @@
  */
 export default class Globals {
   public static backendUrl() {
-    if (!import.meta.env.VITE__BACKEND_URL) {
+    if (!import.meta.env.VITE_BACKEND_URL) {
       console.warn(
         'REACT_APP_BACKEND_URL is not set. Using localhost:3001 as fallback.',
       )
     }
 
-    return import.meta.env.VITE__BACKEND_URL ?? 'http://localhost:3001'
+    return import.meta.env.VITE_BACKEND_URL ?? 'http://localhost:3001'
   }
 }
