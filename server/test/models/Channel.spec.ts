388adf91a90256877fb8eefa503976e62666f5cd
tianhaog-cmu-f24 <tianhaog@andrew.cmu.edu>
2025-03-21 19:59:12 -0700

    test (server): [SEM-2641] Add tests for getPublicChannel for Channel model

--- server/test/models/Channel.spec.ts
+++ server/test/models/Channel.spec.ts
@@ -15,14 +15,26 @@ describe('Channel model', () => {
     }
   })
 
-  it('will return the existing public channel', async () => {
-    const channels = await Channel.find().exec()
-    const publicChannel = await Channel.getPublicChannel()
+  describe('getPublicChannel', () => {
 
-    // prove that no new channels are created
-    expect(await Channel.find().exec()).toStrictEqual(channels)
-    expect(channels[0].id).toBe(publicChannel.id)
-  })
+    it('will return the existing public channel', async () => {
+      const channels = await Channel.find().exec()
+      const publicChannel = await Channel.getPublicChannel()
+
+      // prove that no new channels are created
+      expect(await Channel.find().exec()).toStrictEqual(channels)
+      expect(channels[0].id).toBe(publicChannel.id)
+    })
+
+    it ('will create a new public channel if it does not exist', async () => {
+      await Channel.deleteMany({ name: 'Public' }).exec()
+      const channelsBefore = await Channel.find().exec()
+      const publicChannel = await Channel.getPublicChannel()
+      const channelsAfter = await Channel.find().exec()
+      expect(channelsAfter.length).toBe(channelsBefore.length + 1)
+      expect(publicChannel.name).toBe('Public')
+    });
+  });
 
   afterAll(TestDatabase.close)
 })
