a60e7f3a045f522806b836bd3b94ed3d97159d2c
baiyil-cmu-F24 <baiyil@andrew.cmu.edu>
2025-03-22 23:58:15 -0700

    feat (server): [SEM-2588] Add Patients Schema

---
+++ server/src/models/Patient.ts
@@ -0,0 +1,66 @@
+import mongoose, { Schema } from 'mongoose'
+
+export interface IPatient extends Document {
+  patientId: string
+  name: string
+  nameLower: string
+  nurseId: string
+  hospitalId: string
+  priority: string
+  status: string
+}
+
+export const PatientSchema = new Schema({
+  patientId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  /**
+   * Name of the patient
+   */
+  name: {
+    type: String,
+    required: true,
+  },
+
+  /**
+   * Lowercase name of the patient. Used for searching.
+   */
+  nameLower: {
+    type: String,
+    required: true,
+  },
+
+  /**
+   * Nurse ID, should be A user's _id that is a nurse
+   */
+  nurseId: {
+    type: String,
+  },
+
+  /**
+   * Hospital ID, should be a hospital's _id
+   */
+  hospitalId: {
+    type: String,
+  },
+
+  /**
+   * Priority of the patient, which is a string in enum ['e', 'could_wait', 'dismissed', 'dead']
+   */
+  priority: {
+    type: String,
+    enum: ['e', 'could_wait', 'dismissed', 'dead'],
+  },
+
+  /**
+   * Status of the patient, which is a string in enum ['to_er', 'at_er', 'others']
+   */
+  status: {
+    type: String,
+    enum: ['to_er', 'at_er', 'others'],
+  },
+})
+
+export default mongoose.model<IPatient>('Patient', PatientSchema)
