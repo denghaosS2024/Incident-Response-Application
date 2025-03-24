3f023aa914478948aeb850b44b8bc671493f212f
sonyemehS2024 <155502044+sonyemehS2024@users.noreply.github.com>
2025-03-23 17:06:46 -0700

    feat (client): [SEM-2597] Create Find Hospital Page draft

---
+++ client/src/pages/FindHospital.tsx
@@ -0,0 +1,18 @@
+import Column from '@/components/Column'
+import { Box } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { Draggable, Droppable } from 'react-beautiful-dnd'
+
+const FindHospital: React.FC = () => {
+    return(
+        <>
+        <Box>
+           
+        </Box>
+        </>
+    )
+
+}
+
+
+export default FindHospital
