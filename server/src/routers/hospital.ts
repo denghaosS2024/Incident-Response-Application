d41fa60ce940d80aa510b7393d48a6ee70db295d
baiyil-cmu-F24 <baiyil@andrew.cmu.edu>
2025-03-24 16:49:45 -0700

    feat (server): [SEM-2588] Add more APIs to Patient

--- server/src/routers/hospital.ts
+++ server/src/routers/hospital.ts
@@ -3,15 +3,42 @@ import HospitalController from '../controllers/HospitalController'
 import type { IHospital } from '../models/Hospital'
 
 export default Router()
+  /**
+   * @swagger
+   * /api/hospitals/register:
+   *   post:
+   *     summary: Register a new hospital
+   *     description: Register a new hospital
+   *     tags: [Hospital]
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             required:
+   *               - hospitalName
+   *               - hospitalAddress
+   *             properties:
+   *               hospitalName:
+   *                 type: string
+   *               hospitalAddress:
+   *                 type: string
+   *     responses:
+   *       201:
+   *         description: Hospital registered successfully
+   *       400:
+   *         description: Bad request
+   *       500:
+   *         description: Server error
+   */
   .post('/register', async (request, response) => {
     try {
       const hospitalData = request.body as IHospital
       if (!hospitalData.hospitalName || !hospitalData.hospitalAddress) {
-        return response
-          .status(400)
-          .send({
-            message: 'hospitalName and hospitalAddress are mandatory fields.',
-          })
+        return response.status(400).send({
+          message: 'hospitalName and hospitalAddress are mandatory fields.',
+        })
       }
       const result = await HospitalController.create(hospitalData)
       // TODO : Trigger a socket event here to update all screens
@@ -22,6 +49,27 @@ export default Router()
     }
   })
 
+  /**
+   * @swagger
+   * /api/hospitals:
+   *   get:
+   *     summary: Get all hospitals
+   *     description: Get all hospitals
+   *     tags: [Hospital]
+   *     parameters:
+   *       - in: query
+   *         name: hospitalId
+   *         description: ID of the hospital
+   *         required: false
+   *         type: string
+   *     responses:
+   *       200:
+   *         description: Hospitals retrieved
+   *       404:
+   *         description: No hospital found
+   *       500:
+   *         description: Server error
+   */
   .get('/', async (request, response) => {
     try {
       const { hospitalId } = request.query
