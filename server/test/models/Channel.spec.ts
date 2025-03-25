cd403d315576e24251ffee6c312e5dea6385f93a
baiyil-cmu-F24 <baiyil@andrew.cmu.edu>
2025-03-24 20:42:01 -0700

    fix (server): [--] Channel tests failing due to concurrency issues

--- server/test/models/Channel.spec.ts
+++ server/test/models/Channel.spec.ts
@@ -1,6 +1,6 @@
 import Channel from '../../src/models/Channel'
+import SystemGroupConfigs from '../../src/utils/SystemDefinedGroups'
 import * as TestDatabase from '../utils/TestDatabase'
-import SystemGroupConfigs from "../../src/utils/SystemDefinedGroups";
 
 describe('Channel model', () => {
   jest.setTimeout(10000)
@@ -9,6 +9,11 @@ describe('Channel model', () => {
   it('will get all system defined groups', async () => {
     const channels = await Channel.find().exec()
     expect(channels.length).toBe(SystemGroupConfigs.length)
+
+    //Sort both arrays by name because of bad insertion order
+    channels.sort((a, b) => a.name.localeCompare(b.name))
+    SystemGroupConfigs.sort((a, b) => a.name.localeCompare(b.name))
+
     for (let i = 0; i < channels.length; i++) {
       expect(channels[i].name).toBe(SystemGroupConfigs[i].name)
       expect(channels[i].description).toBe(SystemGroupConfigs[i].description)
@@ -16,25 +21,33 @@ describe('Channel model', () => {
   })
 
   describe('getPublicChannel', () => {
-
-    it('will return the existing public channel', async () => {
+    it('Public channel is created automatically so that getPublicChannel should not implicity create one', async () => {
       const channels = await Channel.find().exec()
-      const publicChannel = await Channel.getPublicChannel()
+
+      // Get the public channel, this can create another channel if it doesn't exist
+      // Which we do not want
+      await Channel.getPublicChannel()
 
       // prove that no new channels are created
-      expect(await Channel.find().exec()).toStrictEqual(channels)
-      expect(channels[0].id).toBe(publicChannel.id)
+      const updatedList = await Channel.find().exec()
+
+      // Sort both arrays by name because of bad insertion order
+      updatedList.sort((a, b) => a.name.localeCompare(b.name))
+      channels.sort((a, b) => a.name.localeCompare(b.name))
+
+      expect(updatedList).toStrictEqual(channels)
+      // expect(updatedList[0].id).toBe(publicChannel.id)
     })
 
-    it ('will create a new public channel if it does not exist', async () => {
+    it('will create a new public channel if it does not exist', async () => {
       await Channel.deleteMany({ name: 'Public' }).exec()
       const channelsBefore = await Channel.find().exec()
       const publicChannel = await Channel.getPublicChannel()
       const channelsAfter = await Channel.find().exec()
       expect(channelsAfter.length).toBe(channelsBefore.length + 1)
       expect(publicChannel.name).toBe('Public')
-    });
-  });
+    })
+  })
 
   afterAll(TestDatabase.close)
 })
