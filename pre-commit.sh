8ef3bb08bb48b45f4e86b09ee17511bcddc60850
baiyil-cmu-F24 <baiyil@andrew.cmu.edu>
2025-03-26 23:53:11 -0700

    fix (client): [--] Client-side ESLint not set up properly

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,13 +1,25 @@
  #!/bin/bash
 
-CHANGED_FILES=$(git diff --cached --name-only --diff-filter=ACM | grep -E "\\.(js|ts|tsx|json|md)$"); 
+CHANGED_FILES=$(git diff --cached --name-only --diff-filter=ACM); 
+PKG_MGR=${PKG_MGR:-npm}
 
 print_banner() {
     echo "========================================"
 }
 
 run_prettier() {
-    if ! npx prettier --write $CHANGED_FILES; then
+    # Only format files that have changed
+
+    FILE_TO_FMT=$(cat $CHANGED_FILES | grep -E "\\.(js|ts|tsx|json|md)$");     
+
+    if [ -z "$FILE_TO_FMT" ]; then
+        echo "No files to format, skipping prettier"
+        return
+    fi
+
+    echo "Files to format: $FILE_TO_FMT"
+
+    if ! prettier --write $FILE_TO_FMT; then
         echo "ERROR: Prettier failed to run"
         print_banner
         exit 1
@@ -15,7 +27,7 @@ run_prettier() {
 }
 
 run_linter() {
-    if ! (cd server && npm run lint && cd ..); then
+    if ! $PKG_MGR run lint; then
         echo "ERROR: Linter failed. Please fix the linting errors and try again."
         print_banner
         exit 1
