6ef617f292e53f8669d69571e6d316e3fc922210
yingdancS2024 <yingdanc@andrew.cmu.edu>
2025-03-27 15:44:36 -0700

    set up router [SEM-2497]

---
+++ client/src/pages/IncidentReportPage.tsx
@@ -0,0 +1,11 @@
+import Step5ResponseTimeline from '../components/IncidentReport/Step5ResponseTimeline.tsx'
+
+const IncidentReportPage = () => {
+  return (
+    <div>
+      <Step5ResponseTimeline />
+    </div>
+  )
+}
+
+export default IncidentReportPage
