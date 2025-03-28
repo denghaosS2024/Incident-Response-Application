be91057f4c4b57ed4b7758d67caf90d3b9e8266d
sonyemehS2024 <155502044+sonyemehS2024@users.noreply.github.com>
2025-03-28 15:47:03 -0700

    feat (client): [SEM-2599] Add Failing Test for FindPatientByHospitalId (TDD)

---
+++ server/test/controllers/PatientController.spec.ts
@@ -0,0 +1,41 @@
+// import { Query, Types } from 'mongoose'
+import PatientController from '../../src/controllers/PatientController'
+import Patient from '../../src/models/Patient'
+import * as TestDatabase from '../utils/TestDatabase'
+
+describe('Patient Controller', () => {
+  beforeAll(TestDatabase.connect)
+  beforeEach(() => jest.clearAllMocks())
+  afterEach(async () => {
+    jest.restoreAllMocks()
+    await Patient.deleteMany({})
+  })
+  afterAll(TestDatabase.close)
+
+  
+  const createTestPatient = async (username: string) => {
+    const rawPatient = new Patient({
+      patientId: `I${username}`,
+      name: 'Zoe E',
+      nameLower: 'zoe',
+      hospitalId: 'hospital123',
+      priority: 'could_wait',
+      status: 'at_er',
+      location: 'ER',
+    })
+
+    return rawPatient.save()
+  }
+
+  it('should be able to get all patients by hospitalID', async () => {
+    await createTestPatient('Zoe')
+    const patients = await PatientController.findByHospitalId('hospital123')
+
+    // expect incidents to be an empty array
+    expect(patients).toBeDefined()
+    expect(patients.length).toBe(1)
+    expect(patients[0].hospitalId).toBe('hospital123')
+    expect(patients[0].name).toBe('Zoe E')
+  })
+
+})
