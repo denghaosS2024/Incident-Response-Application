0a186a199ddf3bf5c81cb632941ce6d8037de035
Gary Chen <honghao2@andrew.cmu.edu>
2025-03-30 01:26:10 -0700

    feat (client): [SEM-2519] add AddressBar for all steps

--- client/src/components/feature/SARTask/SARTaskStep1.tsx
+++ client/src/components/feature/SARTask/SARTaskStep1.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import styles from '../../../styles/SARTaskPage.module.css'
 import SARTaskTitle from './SARTaskTitle.tsx'
+import AddressBar from './AddressBar.tsx'
 
 
 const SARTaskStep1: React.FC = () => {
 
   return (
     <div className={styles.wrapperStep}>
+      <AddressBar address='4400 Forbes Ave, Pittsburgh, PA 15213' /> {/*TODO: load address dynamically*/}
+      <div className="mt-2"></div> {/* add space between components */}
       <SARTaskTitle
         title={'Initial Marker'}
         subtitle={'Draw this marker on the wall, next to the main entrance:'}
