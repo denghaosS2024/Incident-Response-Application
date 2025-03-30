c416e4b51900736442d64f296f6610c74ff5c4e6
Gary Chen <honghao2@andrew.cmu.edu>
2025-03-29 23:42:15 -0700

    feat (client): [SEM-2518] ClickableStepper supports square style step icon

---
+++ client/src/components/SquareStepIconComponent.tsx
@@ -0,0 +1,31 @@
+import {StepIconProps, styled} from '@mui/material'
+
+
+const SquareStepIconComponent = (props: StepIconProps) => {
+  const { active, icon } = props;
+
+  const SquareStepIcon = styled('div')(({}) => ({
+    width: 32,
+    height: 32,
+    borderRadius: 2,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+    transition: 'background-color 0.3s, color 0.3s',
+  }));
+
+  return (
+    <SquareStepIcon
+      style={{
+        backgroundColor: active ? '#1976d2' : '#e0e0e0',
+        color: active ? '#fff' : '#9e9e9e',
+      }}
+    >
+      {icon}
+    </SquareStepIcon>
+  );
+}
+
+export default SquareStepIconComponent
