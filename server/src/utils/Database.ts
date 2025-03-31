118798f6626a6dd76eaaec509d8f09ec612f98e1
Qinruo Yang <qinruoy@andrew.cmu.edu>
2025-03-30 22:29:31 -0700

    fix (server): [--] Fix Read MongoDB URL from ENV

--- server/src/utils/Database.ts
+++ server/src/utils/Database.ts
@@ -24,49 +24,49 @@ dotenv.config({ path: '.env' })
  * @param useTls - Whether to use TLS (default: constructed from environment variables)
  */
 export const connect = async (
-  url: string | undefined = undefined,
-  useTls: boolean | undefined = undefined,
+    url: string | undefined = undefined,
+    useTls: boolean | undefined = undefined,
 ) => {
-  // If MongoDB URL is not provided, use the environment variables
-  if (url === undefined) {
-    let baseUrl = Env.getParam(
-      'MONGODB_URL',
-      'mongodb://localhost:27017',
-      false,
-    )
-    let dbName = Env.getParam('MONGODB_DB_NAME', 'sem', false)
+    // If MongoDB URL is not provided, use the environment variables
+    if (url === undefined) {
+        let baseUrl = Env.getParam(
+            'MONGODB_URL',
+            'mongodb://localhost:27017',
+            false,
+        )
+        let dbName = Env.getParam('MONGODB_DB_NAME', 'sem', false)
 
-    // Edge case for those who added an extra forward slash at either ends
-    if (baseUrl?.endsWith('/')) {
-      baseUrl = baseUrl.slice(0, -1)
-    }
-    if (dbName?.startsWith('/')) {
-      dbName = dbName.slice(1)
-    }
-
-    url = `${baseUrl}`
-  }
-
-  // If TLS is not provided, use the environment variables
-  if (useTls === undefined) {
-    useTls = process.env.MONGODB_TLS === '1'
-  }
-
-  await mongoose.connect(
-    url,
-    useTls
-      ? {
-          tls: true,
+        // Edge case for those who added an extra forward slash at either ends
+        if (baseUrl?.endsWith('/')) {
+            baseUrl = baseUrl.slice(0, -1)
         }
-      : undefined,
-  )
-  await User.ensureSystemUser()
-  await Channel.ensureSystemDefinedGroup()
+        if (dbName?.startsWith('/')) {
+            dbName = dbName.slice(1)
+        }
+
+        url = `${baseUrl}/${dbName}`
+    }
+
+    // If TLS is not provided, use the environment variables
+    if (useTls === undefined) {
+        useTls = process.env.MONGODB_TLS === '1'
+    }
+
+    await mongoose.connect(
+        url,
+        useTls
+            ? {
+                  tls: true,
+              }
+            : undefined,
+    )
+    await User.ensureSystemUser()
+    await Channel.ensureSystemDefinedGroup()
 }
 
 /**
  * Close the MongoDB connection
  */
 export const close = async () => {
-  await mongoose.connection.close()
+    await mongoose.connection.close()
 }
