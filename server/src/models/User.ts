bbbd497f543f92cf2f922a4bf5cc910993e8b520
baiyil-cmu-F24 <baiyil@andrew.cmu.edu>
2025-03-31 22:38:07 +0000

    chore (server): [--] Build demo env

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -4,7 +4,7 @@
  * Represents a user in the system with authentication capabilities.
  */
 
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import mongoose, { Document, Model, Schema } from 'mongoose'
 
 import ROLES from '../utils/Roles'
@@ -15,180 +15,185 @@ const SALT_WORK_FACTOR = 10
  * Interface for the User document
  */
 export interface IUser extends Document {
-  username: string
-  password?: string
-  // phoneNumber?: string
-  role: string
-  assignedCity?: string | null
-  assignedCar?: string | null
-  assignedTruck?: string | null
-  assignedVehicleTimestamp?: Date
-  assignedIncident?: string | null
-  previousLatitude: number
-  previousLongitude: number
-  comparePassword: (candidatePassword: string) => Promise<boolean>
-  hospitalId?: string;
+    username: string
+    password?: string
+    // phoneNumber?: string
+    role: string
+    assignedCity?: string | null
+    assignedCar?: string | null
+    assignedTruck?: string | null
+    assignedVehicleTimestamp?: Date
+    assignedIncident?: string | null
+    previousLatitude: number
+    previousLongitude: number
+    comparePassword: (candidatePassword: string) => Promise<boolean>
+    hospitalId?: string
 }
 
 /**
  * User Schema
  */
 const UserSchema = new Schema<IUser>({
-  username: {
-    type: String,
-    required: true,
-    index: {
-      unique: true,
+    username: {
+        type: String,
+        required: true,
+        index: {
+            unique: true,
+        },
     },
-  },
-  password: { type: String, required: true, select: false },
-  // phoneNumber: { type: String, required: true },
-  role: { type: String, required: true, default: ROLES.CITIZEN },
-  /**
-   * assignedCity should only be used if role is 'Police' or 'Fire'.
-   * Default is null. When role is anything else, assignedCity must remain null.
-   */
-  assignedCity: {
-    type: String,
-    default: null,
-    validate: {
-      validator: function (this: IUser, val: string | null) {
-        // If role is Police or Fire, assignedCity can be any string (including null).
-        // Otherwise, assignedCity must be null.
-        if (this.role === ROLES.POLICE || this.role === ROLES.FIRE) {
-          return true
-        }
-        return val === null
-      },
-      message: 'assignedCity is only allowed for users with role Police or Fire.',
+    password: { type: String, required: true, select: false },
+    // phoneNumber: { type: String, required: true },
+    role: { type: String, required: true, default: ROLES.CITIZEN },
+    /**
+     * assignedCity should only be used if role is 'Police' or 'Fire'.
+     * Default is null. When role is anything else, assignedCity must remain null.
+     */
+    assignedCity: {
+        type: String,
+        default: null,
+        validate: {
+            validator: function (this: IUser, val: string | null) {
+                // If role is Police or Fire, assignedCity can be any string (including null).
+                // Otherwise, assignedCity must be null.
+                if (this.role === ROLES.POLICE || this.role === ROLES.FIRE) {
+                    return true
+                }
+                return val === null
+            },
+            message:
+                'assignedCity is only allowed for users with role Police or Fire.',
+        },
     },
-  },
-  /**
-   * assignedCar should only be used if role is 'Police'.
-   * Default is null. When role is anything else, assignedCar must remain null.
-   */
-  assignedCar: {
-    type: String,
-    default: null,
-    validate: {
-      validator: function (this: IUser, val: string | null) {
-        // If role is Police, assignedCar can be any string (including null).
-        // Otherwise, assignedCar must be null.
-        if (this.role === ROLES.POLICE) {
-          return true
-        }
-        return val === null
-      },
-      message: 'assignedCar is only allowed for users with role Police.',
+    /**
+     * assignedCar should only be used if role is 'Police'.
+     * Default is null. When role is anything else, assignedCar must remain null.
+     */
+    assignedCar: {
+        type: String,
+        default: null,
+        validate: {
+            validator: function (this: IUser, val: string | null) {
+                // If role is Police, assignedCar can be any string (including null).
+                // Otherwise, assignedCar must be null.
+                if (this.role === ROLES.POLICE) {
+                    return true
+                }
+                return val === null
+            },
+            message: 'assignedCar is only allowed for users with role Police.',
+        },
     },
-  },
-  /**
-   * assignedTruck should only be used if role is 'Fire'.
-   * Default is null. When role is anything else, assignedTruck must remain null.
-   */
-  assignedTruck: {
-    type: String,
-    default: null,
-    validate: {
-      validator: function (this: IUser, val: string | null) {
-        // If role is Fire, assignedTruck can be any string (including null).
-        // Otherwise, assignedTruck must be null.
-        if (this.role === ROLES.FIRE) {
-          return true
-        }
-        return val === null
-      },
-      message: 'assignedTruck is only allowed for users with role Fire.',
+    /**
+     * assignedTruck should only be used if role is 'Fire'.
+     * Default is null. When role is anything else, assignedTruck must remain null.
+     */
+    assignedTruck: {
+        type: String,
+        default: null,
+        validate: {
+            validator: function (this: IUser, val: string | null) {
+                // If role is Fire, assignedTruck can be any string (including null).
+                // Otherwise, assignedTruck must be null.
+                if (this.role === ROLES.FIRE) {
+                    return true
+                }
+                return val === null
+            },
+            message: 'assignedTruck is only allowed for users with role Fire.',
+        },
     },
-  },
-  /**
-   * assignedVehicleTimestamp should only be used if role is 'Police' or 'Fire'.
-   * Default is null. When role is anything else, assignedVehicleTimestamp must remain null.
-   */
-  assignedVehicleTimestamp: {
-    type: Date,
-    default: null,
-    validate: {
-      validator: function (this: IUser, val: Date | null) {
-        // If role is Police or Fire, assignedVehicleTimestamp can be any date (including null).
-        // Otherwise, assignedVehicleTimestamp must be null.
-        if (this.role === ROLES.POLICE || this.role === ROLES.FIRE) {
-          return true
-        }
-        return val === null
-      },
-      message: 'assignedVehicleTimestamp is only allowed for users with role Police or Fire.',
+    /**
+     * assignedVehicleTimestamp should only be used if role is 'Police' or 'Fire'.
+     * Default is null. When role is anything else, assignedVehicleTimestamp must remain null.
+     */
+    assignedVehicleTimestamp: {
+        type: Date,
+        default: null,
+        validate: {
+            validator: function (this: IUser, val: Date | null) {
+                // If role is Police or Fire, assignedVehicleTimestamp can be any date (including null).
+                // Otherwise, assignedVehicleTimestamp must be null.
+                if (this.role === ROLES.POLICE || this.role === ROLES.FIRE) {
+                    return true
+                }
+                return val === null
+            },
+            message:
+                'assignedVehicleTimestamp is only allowed for users with role Police or Fire.',
+        },
     },
-  },
-  previousLatitude: { type: Number, required: false, default: 0 },
-  previousLongitude: { type: Number, required: false, default: 0 },
-  hospitalId: { type: String, required: false, ref: 'Hospital' },
-  __v: { type: Number, select: false },
+    previousLatitude: { type: Number, required: false, default: 0 },
+    previousLongitude: { type: Number, required: false, default: 0 },
+    hospitalId: { type: String, required: false, ref: 'Hospital' },
+    __v: { type: Number, select: false },
 })
 
 /**
  * Pre-save hook to hash the password before saving the user to the database
  */
 UserSchema.pre('save', function (next) {
-  const user = this as IUser
+    const user = this as IUser
 
-  if (!user.isModified('password')) return next()
+    if (!user.isModified('password')) return next()
 
-  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-    if (err) return next(err)
+    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+        if (err) return next(err)
 
-    bcrypt.hash(user.password!, salt, (err, hash) => {
-      if (err) return next(err)
+        bcrypt.hash(user.password!, salt ?? 10, (err, hash) => {
+            if (err) return next(err)
 
-      // overwrite the plaintext password with the hashed one
-      user.password = hash
-      next()
+            // overwrite the plaintext password with the hashed one
+            user.password = hash
+            next()
+        })
     })
-  })
 })
 
 /**
  * Method to compare a given password with the user's hashed password
  */
 UserSchema.methods.comparePassword = function (candidatePassword) {
-  return new Promise((resolve, reject) => {
-    bcrypt.compare(candidatePassword, this.password!, (error, isMatch) => {
-      if (error) {
-        reject(error)
-      } else {
-        resolve(isMatch)
-      }
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(candidatePassword, this.password!, (error, isMatch) => {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(isMatch)
+            }
+        })
     })
-  })
 }
 
 /**
  * Method to ensure a System Administator exits in the database.
  */
 UserSchema.statics.ensureSystemUser = async function () {
-  const username: string = "System"
-  const password: string = "1234"
-  const role = ROLES.ADMINISTRATOR
+    const username: string = 'System'
+    const password: string = '1234'
+    const role = ROLES.ADMINISTRATOR
 
-  try {
-    const existingUser = await this.findOne({ username: username })
+    try {
+        const existingUser = await this.findOne({ username: username })
 
-    if (!existingUser) {
-      console.log('System user does not exist. Creating now.')      
-      // Create user using new model instance to trigger pre-save hook
-      const systemUser = new this({
-        username: username,
-        password: password,  // Plain password - will be hashed by pre-save hook
-        role: role
-      })
-      await systemUser.save()
-      console.log('System user created successfully.');
-    } else {
-      console.log('System user already exists.')
+        if (!existingUser) {
+            console.log('System user does not exist. Creating now.')
+            // Create user using new model instance to trigger pre-save hook
+            const systemUser = new this({
+                username: username,
+                password: password, // Plain password - will be hashed by pre-save hook
+                role: role,
+            })
+            await systemUser.save()
+            console.log('System user created successfully.')
+        } else {
+            console.log('System user already exists.')
+        }
+    } catch (error) {
+        console.error('Error creating system user:', error)
     }
-  } catch (error) {
-    console.error('Error creating system user:', error)
-  }
 }
 
-export default mongoose.model<IUser, Model<IUser> & { ensureSystemUser: () => Promise<void> }>('User', UserSchema)
+export default mongoose.model<
+    IUser,
+    Model<IUser> & { ensureSystemUser: () => Promise<void> }
+>('User', UserSchema)
--- server/test/models/User.spec.ts
+++ server/test/models/User.spec.ts
@@ -1,71 +1,77 @@
-import bcrypt from 'bcrypt'
-import * as TestDatabase from '../utils/TestDatabase'
+import bcrypt from 'bcryptjs'
 import User from '../../src/models/User'
+import * as TestDatabase from '../utils/TestDatabase'
 
 describe('User model', () => {
-  // "System" user is created in the database upon app run so by default there always is one user present in the database.
-  beforeAll(TestDatabase.connect)
-  beforeEach(() => jest.clearAllMocks())
-  afterEach(() => jest.restoreAllMocks())
+    // "System" user is created in the database upon app run so by default there always is one user present in the database.
+    beforeAll(TestDatabase.connect)
+    beforeEach(() => jest.clearAllMocks())
+    afterEach(() => jest.restoreAllMocks())
 
-  const createTestUser = async (username: string, password: string, phoneNumber) => {
-    const rawUser = await new User({
-      username,
-      password,
-      phoneNumber
+    const createTestUser = async (
+        username: string,
+        password: string,
+        phoneNumber,
+    ) => {
+        const rawUser = await new User({
+            username,
+            password,
+            phoneNumber,
+        })
+
+        return rawUser.save()
+    }
+
+    it("will encrypt user's password", async () => {
+        const rawUser = await createTestUser('User-1', 'password', '1234567890')
+
+        expect(rawUser.password).toBeDefined()
+        expect(rawUser.password).not.toEqual('password')
     })
 
-    return rawUser.save()
-  }
+    it('will hide passwords and versions in queries', async () => {
+        const users = await User.find().exec()
 
-  it("will encrypt user's password", async () => {
-    const rawUser = await createTestUser('User-1', 'password', '1234567890')
+        expect(users.length).toBe(2)
 
-    expect(rawUser.password).toBeDefined()
-    expect(rawUser.password).not.toEqual('password')
-  })
+        const user = users[0]
 
-  it('will hide passwords and versions in queries', async () => {
-    const users = await User.find().exec()
-
-    expect(users.length).toBe(2)
-
-    const user = users[0]
-
-    expect(user.password).not.toBeDefined()
-    expect(user.__v).not.toBeDefined()
-  })
-
-  it('compares clear-text password with encrypted password', async () => {
-    const rawUser = await createTestUser('User-2', 'password', '0987654321')
-
-    expect(await rawUser.comparePassword('some random string')).toBeFalsy()
-    expect(await rawUser.comparePassword('password')).toBeTruthy()
-  })
-
-  it('handles invalid hashes in comparePassword method', async () => {
-    const rawUser = await createTestUser('User-3', 'password', '1357924680')
-
-    // Replace the password with an invalid hash
-    rawUser.password = 'invalid_hash'
-    await rawUser.save()
-
-    const result = await rawUser.comparePassword('password')
-    expect(result).toBeFalsy()
-  })
-
-  it('handles errors in comparePassword method', async () => {
-    const rawUser = await createTestUser('User-4', 'password', '1234567890')
-
-    // Mock bcrypt.compare to simulate an error
-    jest.spyOn(bcrypt, 'compare').mockImplementationOnce((_, __, callback) => {
-      callback(new Error('Mocked bcrypt error'), false)
+        expect(user.password).not.toBeDefined()
+        expect(user.__v).not.toBeDefined()
     })
 
-    await expect(rawUser.comparePassword('password')).rejects.toThrow(
-      'Mocked bcrypt error',
-    )
-  })
+    it('compares clear-text password with encrypted password', async () => {
+        const rawUser = await createTestUser('User-2', 'password', '0987654321')
 
-  afterAll(TestDatabase.close)
+        expect(await rawUser.comparePassword('some random string')).toBeFalsy()
+        expect(await rawUser.comparePassword('password')).toBeTruthy()
+    })
+
+    it('handles invalid hashes in comparePassword method', async () => {
+        const rawUser = await createTestUser('User-3', 'password', '1357924680')
+
+        // Replace the password with an invalid hash
+        rawUser.password = 'invalid_hash'
+        await rawUser.save()
+
+        const result = await rawUser.comparePassword('password')
+        expect(result).toBeFalsy()
+    })
+
+    it('handles errors in comparePassword method', async () => {
+        const rawUser = await createTestUser('User-4', 'password', '1234567890')
+
+        // Mock bcrypt.compare to simulate an error
+        jest.spyOn(bcrypt, 'compare').mockImplementationOnce(
+            (_, __, callback) => {
+                callback(new Error('Mocked bcrypt error'), false)
+            },
+        )
+
+        await expect(rawUser.comparePassword('password')).rejects.toThrow(
+            'Mocked bcrypt error',
+        )
+    })
+
+    afterAll(TestDatabase.close)
 })
