ede48d931f7210ce3dcb5540e70852b97bd63b61
Paul Kotchavong <pkotchav@andrew.cmu.edu>
2025-03-30 23:51:50 -0700

    test (server): [SEM-2612] Red Cycle: set and get group alert state

--- server/test/services/AlertService.spec.ts
+++ server/test/services/AlertService.spec.ts
@@ -1,3 +1,4 @@
+import { GroupAlertState } from '../../src/models/AlertQueue';
 import AlertService from '../../src/services/AlertService';
 
 describe('AlertService', () => {
@@ -13,6 +14,21 @@ describe('AlertService', () => {
         const alertService = AlertService;
         const groupAlertState = alertService.getGroupAlertState(groupId);
         expect(groupAlertState).toBeUndefined();
+    });
+
+    it('should set and get group alert state', () => {
+        const groupId = '123';
+        const alertService = AlertService;
+
+        const testGroupAlertState: GroupAlertState = {
+            alertQueue: [],
+            ongoingAlert: undefined,
+            timeoutHandle: undefined,
+        };
+
+        alertService.setGroupAlertState(groupId, testGroupAlertState);
+        const groupAlertState = alertService.getGroupAlertState(groupId);
+        expect(groupAlertState).toEqual(testGroupAlertState);
     });
   });
 
