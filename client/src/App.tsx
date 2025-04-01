959eb7e4329f97bb6c2f88e23c822cd726bac272
taomingl-cmu-F24 <taomingl@andrew.cmu.edu>
2025-04-01 01:08:27 -0700

    feat (client): [SEM-2578] navigate from patient page to visit log page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -97,7 +97,7 @@ export default function App() {
                             element={<PatientAdmitPage />}
                         />
                         <Route
-                            path="/patient-visit"
+                            path="/patient-visit/:username"
                             element={<PatientVisitPage />}
                         />
                         <Route
