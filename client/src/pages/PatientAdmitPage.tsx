1931e968cc56840dac86ceeeed8f7d4c333b3630
taomingl-cmu-F24 <taomingl@andrew.cmu.edu>
2025-03-31 21:41:19 -0700

    fix (client): [--] Use PatientInforForm in patient page

--- client/src/pages/PatientAdmitPage.tsx
+++ client/src/pages/PatientAdmitPage.tsx
@@ -1,4 +1,4 @@
-import PatientForm from '@/components/feature/Reach911/PatientForm'
+import PatientInforForm from '@/components/feature/Reach911/PatientInforForm'
 import { Box } from '@mui/material'
 import React from 'react'
 import { useSearchParams } from 'react-router-dom'
@@ -6,10 +6,10 @@ import { useSearchParams } from 'react-router-dom'
 const PatientAdmitPage: React.FC = () => {
     const [searchParams] = useSearchParams()
     const username = searchParams.get('username') || undefined
-    
+
     return (
         <Box sx={{ height: '100%' }}>
-            <PatientForm username={username}></PatientForm>
+            <PatientInforForm username={username}></PatientInforForm>
         </Box>
     )
 }
