ba7bdba05f1097a27285974c10aa139254342413
baiyil-cmu-F24 <baiyil@andrew.cmu.edu>
2025-04-02 19:47:50 -0700

    chore (common): [--] Commited wrong prettierrc by accident

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-    singleQuote: true,
-    trailingComma: 'all',
-    semi: false,
-    tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: false,
+  tabWidth: 4,
 }
