d240d2804d3f81f2f97778997d1a51273c66662a
Paul Kotchavong <pkotchav@andrew.cmu.edu>
2025-04-20 17:59:33 -0700

    test (server): [SEM-2737] Red Cycle: error handling in addFollowUp

--- server/test/controllers/MissingPersonFollowUpController.spec.ts
+++ server/test/controllers/MissingPersonFollowUpController.spec.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import MissingPersonFollowUpController from "../../src/controllers/MissingPersonFollowUpController";
-import { IMissingFollowUpBase } from "../../src/models/MissingFollowUp";
+import MissingFollowUp, { IMissingFollowUpBase } from "../../src/models/MissingFollowUp";
 import * as TestDatabase from "../utils/TestDatabase";
 
 describe("MissingPersonFollowUpController", () => {
@@ -23,5 +23,23 @@ describe("MissingPersonFollowUpController", () => {
     expect(followUp).toBeDefined();
     expect(followUp.isSpotted).toBe(true);
     expect(followUp._id).toBeDefined();
-  })
+  });
+
+  it('throw error by mongoDB', async() => {
+
+    jest.spyOn(MissingFollowUp.prototype, 'save').mockRejectedValueOnce(new Error("DB save failed")
+);
+
+    const newFollowUp: IMissingFollowUpBase = {
+        reportId: new mongoose.Types.ObjectId(),  
+        isSpotted: true,
+        locationSpotted: "123 South Akron Rd. Mountain View, CA 94075",
+        datetimeSpotted: new Date("2025-10-28T11:45"),
+        additionalComment: "Random comment",
+      };
+
+      await expect(
+        MissingPersonFollowUpController.addFollowUp(newFollowUp)
+      ).rejects.toThrow("DB save failed");
+  });
 })
