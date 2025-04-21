4021e85f5b0d92d05025c39d05890d2aff13ce66
Paul Kotchavong <pkotchav@andrew.cmu.edu>
2025-04-20 17:15:02 -0700

    test (server): [SEM-2737] Green Cycle: missing person addFollowUp nominal

--- server/test/controllers/MissingPersonFollowUpController.spec.ts
+++ server/test/controllers/MissingPersonFollowUpController.spec.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import MissingPersonFollowUpController from "../../src/controllers/MissingPersonFollowUpController";
 import { IMissingFollowUpBase } from "../../src/models/MissingFollowUp";
 import * as TestDatabase from "../utils/TestDatabase";
 
@@ -12,12 +13,12 @@ describe("MissingPersonFollowUpController", () => {
         reportId: new mongoose.Types.ObjectId(),
         isSpotted: true, 
         locationSpotted: "123 South Akron Rd. Mountain View, CA 94075",
-        datetimeSpotted: new Date("10-27-2025T19:30"),
+        datetimeSpotted: new Date("2025-10-27T19:30"),
         additionalComment: "addtional comment"
 
     } ;
 
-    const followUp = new MissingPersonFollowUpController.addFollowUp(newFollowUp)
+    const followUp = await MissingPersonFollowUpController.addFollowUp(newFollowUp);
 
     expect(followUp).toBeDefined();
     expect(followUp.isSpotted).toBe(true);
