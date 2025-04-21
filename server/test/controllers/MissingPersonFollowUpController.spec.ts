8e39a9b3a9360c8e9b8a6f8ebe131c5112ef1aa6
Paul Kotchavong <pkotchav@andrew.cmu.edu>
2025-04-20 19:57:00 -0700

    test (server): [SEM-2734] Red Cycle: get all followups for specific reportId nominal

--- server/test/controllers/MissingPersonFollowUpController.spec.ts
+++ server/test/controllers/MissingPersonFollowUpController.spec.ts
@@ -42,4 +42,22 @@ describe("MissingPersonFollowUpController", () => {
         MissingPersonFollowUpController.addFollowUp(newFollowUp)
       ).rejects.toThrow("Failed to add Follow Up info");
   });
+
+  it("should return all followups for specific report", async() => {
+    const newFollowUp: IMissingFollowUpBase = {
+        reportId: new mongoose.Types.ObjectId(),
+        isSpotted: true, 
+        locationSpotted: "123 South Akron Rd. Mountain View, CA 94075",
+        datetimeSpotted: new Date("2025-10-27T19:30"),
+        additionalComment: "additional comment"
+
+    };
+
+    const followUp = await MissingPersonFollowUpController.addFollowUp(newFollowUp);
+
+    const reportFollowUpList = await MissingPersonFollowUpController.getAllFollowUpsByReportId(followUp.reportId.toString());
+
+    expect(reportFollowUpList.length).toBeGreaterThan(0);
+    expect(reportFollowUpList[0].additionalComment).toBe("additional comment");
+  })
 })
