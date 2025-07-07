4671b4acf4dc404908c416ebe58793b20d035784
SimonSun22 <84614775+SimonSun22@users.noreply.github.com>
2025-07-07 03:47:10 -0700

    Fix: use Render's dynamic PORT env var

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,14 +8,14 @@
 import { Server as SocketIO } from "socket.io";
 
 import * as Http from "node:http";
+import * as TestDatabase from "../test/utils/TestDatabase";
 import app from "./app";
 import Socket from "./socket";
 import * as Database from "./utils/Database";
 import Env from "./utils/Env";
-import * as TestDatabase from "../test/utils/TestDatabase";
 
 // Set the port for the server to listen on
-const PORT = parseInt(Env.getParam("PORT", "3001"));
+const PORT = parseInt(process.env.PORT || Env.getParam("PORT", "3001"));
 
 async function getHttpServer() {
   return Http.createServer(app);
